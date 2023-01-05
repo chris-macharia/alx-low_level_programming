9c27e3157fb0b1f48026556d96630694001dbf65
chris-macharia <christophermaina003@gmail.com>
2023-01-05 08:28:55 +0000

    Inception, is it possible?

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,8 +9,9 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int _sqrt(int n, int i);
-int wildcmp(char *s1, char *s2);
 int is_prime_number(int n);
+int divisors(int n, int m);
+int wildcmp(char *s1, char *s2);
 int is_palindrome(char *s);
 
 #endif
